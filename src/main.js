fa89b384f2e5237be57de0d2ce4bef22bff1f399
FrandelDev <frandel-c.r@outlook.com>
2023-07-07 11:10:57 -0400

    busquedas de peliculas y categorias

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const trendingList = document.querySelector("#trendingPreview .trendingPreview-movieList");
-const  categories = document.querySelector("#categoriesPreview .categoriesPreview-list")
+const  categories = document.querySelector("#categoriesPreview .categoriesPreview-list");
+const  categoryList = document.querySelector(".categories-list .category-container");
+const  filterList = document.querySelector("#genericList");
 
-const BASE_API = "https://api.themoviedb.org/3";
 
 const api = axios.create({
     baseURL: "https://api.themoviedb.org/3",
@@ -17,7 +18,7 @@ const api = axios.create({
 
 async function getTrendingMovies(){
     const {data} = await api("/trending/movie/day"); 
-
+    trendingList.innerHTML = '';
 data.results.forEach(movie => {
     trendingList.innerHTML += `
     <div class="movie-container">
@@ -32,8 +33,8 @@ data.results.forEach(movie => {
         
 }
 async function getCategories(){
-    const {data} = await api(BASE_API+"/genre/movie/list");
-
+    const {data} = await api("/genre/movie/list");
+    categories.innerHTML = '';
 data.genres.forEach(category => {
 
     categories.innerHTML += `
@@ -41,13 +42,52 @@ data.genres.forEach(category => {
 <h3 id="${"id"+category.id}" class="category-title">${category.name}</h3>
 </div>
 `
-
+ document.querySelector(`#id${category.id}`).addEventListener('click', () => {
+    location.hash = `#category=${category.id}-${category.name}`;
+  });
 });
         
 }
-getCategories();
-getTrendingMovies();
+async function getMoviesByCategory(categoryId){
+   const {data} =  await api('/discover/movie',{
+    params:{
+        "with_genres":categoryId
+    }
+   });
+   filterList.innerHTML='';
+   data.results.forEach(movie =>{
+    filterList.innerHTML += `
+   <div class="movie-container">
+<img
+        src="${'https://image.tmdb.org/t/p/w300'+movie.poster_path}"
+        class="movie-img"
+        alt="${movie.name}"
+      />
+</div>
+   `
+   })
+   
+}
 
-
+async function searchMovie(query){
+    const {data} = await api("/search/movie",{
+        params: {
+            query: query
+        }
+    });
+    console.log(data);
+    filterList.innerHTML = '';
+    data.results.forEach(movie =>{
+        filterList.innerHTML += `
+        <div class="movie-container">
+<img
+        src="${'https://image.tmdb.org/t/p/w300'+movie.poster_path}"
+        class="movie-img"
+        alt="${movie.name}"
+      />
+</div>
+        `
+    });
+}
 
 
